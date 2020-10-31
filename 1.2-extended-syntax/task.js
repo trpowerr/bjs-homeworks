84330b525a61b7efcf8f65c24ea3a91aa300a9bd
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-10-31 15:52:56 +0300

    ver 1.2.1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,11 +1,40 @@
 function getResult(a,b,c){
     // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+		const x = [];
+		let result = null;
+        let disc = b ** 2 - 4 * a * c;
+        if (disc === 0) { 
+			result = (-b) / (2 * a);
+			x.push(result);
+        } else if (disc > 0) {
+			result = ((-b) + Math.sqrt(disc)) / 2 * a;
+			x.push(result);
+			result = ((-b) - Math.sqrt(disc)) / 2 * a;
+			x.push(result);
+		}
+		return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+	// код для задачи №2 писать здесь
+	let num = 0;
+  	let averageMark = 0;
+	let numberValues = marks.length;
+  	if (numberValues === 0) {
+    	return averageMark;
+  	}
+  	if (numberValues > 5) {
+    	marks = marks.slice(0, 5);
+    	numberValues = marks.length;
+    }
+  	for (let value of marks ) {
+    	num += value;
+  	}
+  
+  	averageMark = num / numberValues;
+  
+  	return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
