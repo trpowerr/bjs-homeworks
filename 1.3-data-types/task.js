de7dec32b995c34b1f95dbff8dfe90f137a2baf1
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-11-05 11:58:26 +0300

    hw 1.3.1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,35 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+	const arrArgumentsFun = ['percent', 'contribution', 'amount', 'date']; 
+	const arrControlValue = [];
+	arrControlValue.push(percent, contribution, amount, date);
+	for (let i = 0; i < arrControlValue.length - 1; i++) {
+		if (isNaN(arrControlValue[i])) {
+			return `Параметр <${arrArgumentsFun[i]}> содержит неправильное значение <${arrControlValue[i]}>`;
+		}
+	}
+	if ((amount - contribution) <= 0) {
+		return 0;
+	}
+	let bodyCredit = amount - contribution;
+	let monthNum = ((new Date(date).getFullYear() - new Date().getFullYear()) * 12) - Math.abs(new Date().getMonth() - new Date(date).getMonth());
+	
+	let paymentMonth = bodyCredit * ((percent / 1200)+(percent / 1200)/(((1+(percent / 1200)) ** monthNum)-1));
+	
+	
+	let totalAmount = Number.parseFloat((paymentMonth * monthNum).toFixed(2));
+	console.log(totalAmount)
+	
+  return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name) {
+		name = 'Аноним'
+	}
+	
+	console.log(`Привет, мир! Меня зовут ${name}.`)
+  	return `Привет, мир! Меня зовут ${name}.`;
 }
