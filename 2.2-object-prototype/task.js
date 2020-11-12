7e608f47a064617b2525e409a505e5410a7751f1
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-11-12 16:58:21 +0300

    ver 2.2.1
    task 1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,15 @@
-//String.prototype.isPalindrome - для задачи №1
+//Задача 1
+
+String.prototype.isPalindrome = function () {
+  let str = this.toLowerCase().replace( /\s/g, '');
+  const strReverse = str.split('').reverse().join('').replace( /\s/g, '');
+  if (str === strReverse) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
