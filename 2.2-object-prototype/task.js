aeaccac346ba24ad76254c7523e4dbba78f59be4
Tryakin Mikhail <mixazbubl@yandex.ru>
2020-11-12 18:09:08 +0300

    edits code

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -3,11 +3,7 @@
 String.prototype.isPalindrome = function () {
   let str = this.toLowerCase().replace( /\s/g, '');
   const strReverse = str.split('').reverse().join('').replace( /\s/g, '');
-  if (str === strReverse) {
-    return true;
-  } else {
-    return false;
-  }
+  return str === strReverse
 }
 
 //Задача 2
